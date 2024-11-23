232a1e31afbb7ab36af880f2eb5abe54c95ed1ba
JustinMassart <justin.massart@student.hepl.be>
2024-11-23 11:51:37 +0100

    Added main layout

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Test</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!--    Left Sidebar    -->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!--    Main Content    -->
+          <!--    Right Sidebar    -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+const darkMode = ref(false);
+</script>
