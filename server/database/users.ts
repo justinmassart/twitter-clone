3e6ff9d047c850e6b9236009b5db6cb6552a83ad
JustinMassart <justin.massart@student.hepl.be>
2024-11-23 16:55:26 +0100

    Added users DB functions

---
+++ server/database/users.ts
@@ -0,0 +1,28 @@
+import { prisma } from ".";
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+export const createUser = (userData: Prisma.UserCreateInput) => {
+  const finalUserData = {
+    ...userData,
+    password: bcrypt.hashSync(userData.password, 10),
+  };
+
+  return prisma.user.create({
+    data: finalUserData,
+    omit: {
+      id: true,
+      password: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+export const getUserByUsername = (username: string) => {
+  return prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+};
