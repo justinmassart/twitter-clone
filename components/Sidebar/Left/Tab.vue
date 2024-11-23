09820a2882b86e44293c562130a88c177fba4e2e
JustinMassart <justin.massart@student.hepl.be>
2024-11-23 11:51:53 +0100

    Added Left Sidebar w/ links

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+  <nuxt-link
+    :class="defaultTransition"
+    class="flex items-center p-3 w-min text-black hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white rounded-full"
+    to="#"
+  >
+    <div class="w-6 h-6 text-dark">
+      <slot name="icon"></slot>
+    </div>
+    <div :class="textClasses" class="ml-4 text-xl hidden xl:block">
+      <slot name="name"></slot>
+    </div>
+  </nuxt-link>
+</template>
+<script lang="ts" setup>
+import useTailwindConfig from "~/components/composables/useTailwindConfig";
+
+const { defaultTransition } = useTailwindConfig();
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+const textClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal",
+);
+</script>
