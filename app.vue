c6507d4221885fdc539fc4517869c597bd498210
JustinMassart <justin.massart@student.hepl.be>
2024-11-23 14:04:47 +0100

    Added first part of main content

--- app.vue
+++ app.vue
@@ -10,11 +10,9 @@
               <SidebarLeft />
             </div>
           </div>
-          <!--    Main Content    -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>hey</h1>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
           </main>
-          <!--    Right Sidebar    -->
           <div class="hidden md:block xl:col-span-4 md:col-span-3">
             <div class="sticky top-0">
               <SidebarRight />
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const isLoading = ref(false);
+</script>
+
+<template>
+  <div>
+    <MainSection :loading="isLoading" title="Home"> Content </MainSection>
+  </div>
+</template>
