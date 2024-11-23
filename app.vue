8035fbf69e6deebaa15a2a8d5abeea36e8170c57
JustinMassart <justin.massart@student.hepl.be>
2024-11-23 12:46:19 +0100

    Added main content & right sidebar to base layout

--- app.vue
+++ app.vue
@@ -5,14 +5,21 @@
         <div
           class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
-          <!--    Left Sidebar    -->
           <div class="hidden md:block xs-col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
           </div>
           <!--    Main Content    -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>hey</h1>
+          </main>
           <!--    Right Sidebar    -->
+          <div class="hidden md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
         </div>
       </div>
     </div>
