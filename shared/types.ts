ffc12f8173ae56e2bec3c5092aa410d1a5894241
JustinMassart <justin.massart@student.hepl.be>
2024-11-28 16:48:09 +0100

    Added shared types

---
+++ shared/types.ts
@@ -0,0 +1,27 @@
+export interface TransformedMediaFile {
+  url: string;
+}
+
+export interface TransformedUser {
+  id: string;
+  name: string;
+  email: string;
+  username: string;
+  avatar: string | null;
+  handle: string;
+}
+
+export interface TransformedTweet {
+  id: string;
+  text: string;
+  mediaFiles: TransformedMediaFile[];
+  author: TransformedUser;
+  replies: TransformedTweet[];
+  replyTo: TransformedTweet | null;
+  repliesCount: number;
+  postedAtHuman: string;
+}
+
+export interface TweetsResponse {
+  tweets: TransformedTweet[];
+}
