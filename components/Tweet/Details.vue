162a2b18ae26781ec1fb5b019f2be0f877b354dd
JustinMassart <justin.massart@student.hepl.be>
2024-12-02 15:49:27 +0100

    Added single tweet page content

---
+++ components/Tweet/Details.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { TransformedTweet, TransformedUser, UserType } from '~/shared/types';
+
+defineProps<{
+    tweet: TransformedTweet;
+    user: any;
+}>()
+
+function handleFormSuccess(tweet: TransformedTweet) {
+    navigateTo({
+        path: `/status/${tweet.id}`
+    })
+}
+</script>
+
+<template>
+    <div>
+        <TweetItem :tweet />
+        <TweetForm :user placeholder="Tweet your reply !" :reply-to="tweet" @on-success="handleFormSuccess" />
+        <TweetListFeed :tweets="tweet.replies ?? []" />
+    </div>
+</template>
