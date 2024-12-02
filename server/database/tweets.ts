f7f54045be34e09bf65cc3ebc81ca10944d8a86d
JustinMassart <justin.massart@student.hepl.be>
2024-12-02 15:48:45 +0100

    Updated returned data to include more content

--- server/database/tweets.ts
+++ server/database/tweets.ts
@@ -4,11 +4,24 @@ import { Prisma } from "@prisma/client";
 export const createTweet = (tweetData: Prisma.TweetCreateInput) => {
   return prisma.tweet.create({
     data: tweetData,
+    include: {
+      author: true,
+    },
   });
 };
 
 export const getTweets = (params: {} = {}) => {
   return prisma.tweet.findMany({
     ...params,
+  });
+};
+
+export const getTweetById = (tweetId: string, params: any = {}) => {
+  return prisma.tweet.findUnique({
+    ...params,
+    where: {
+      ...params.where,
+      id: tweetId,
+    },
   });
 };
